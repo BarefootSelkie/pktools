1e3625b3ec1a4f73a770fae257696c3af4897de9
Barefoot Selkie <17512788+BarefootSelkie@users.noreply.github.com>
2024-03-02 11:43:24 +0000

    testing sorting

--- testing.py
+++ testing.py
@@ -7,7 +7,10 @@ pktools.pullPeriodic()
 
 print("Current time : " + pktools.hsTimeHuman(pktools.hsTimeNow()) + "\n")
 
-for member in pktools.memberSeen:
+sorted_tuples = sorted(pktools.memberSeen.items(), key=lambda item: item[1]["lastIn"])
+sorted_dict = {k: v for k, v in sorted_tuples}
+
+for member in sorted_dict:
     who = pktools.getMember(member)["name"]
 
     print(str(who) + ": " + str(pktools.rsLastSeen(member)) + " = " + pktools.hsTimeShort(pktools.hsLastSeen(member)))
@@ -18,9 +21,9 @@ for id in pktools.lastSwitch["members"]:
     print(member["name"] + ": " + str(pktools.rsSinceLastIn(id)) + " = " + str(pktools.hsSinceLastIn(id)))
 print("\nWatching for new fronters:")
 
-while True:
+""" while True:
     if pktools.pullPeriodic():
         for id in pktools.lastSwitch["members"]:
             member = pktools.getMember(id)
             print(member["name"] + ": " + str(pktools.rsSinceLastIn(id)) + " = " + str(pktools.hsSinceLastIn(id)))
-    time.sleep(10)
+    time.sleep(10) """
